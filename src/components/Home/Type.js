05592a1610bba54fc052b586a14091063b37695a
dwarakanathvarunkumar <dwarakanatvarunkumar@gmail.com>
2021-03-22 02:11:18 +0530

    dwaraka pages

---
+++ src/components/Home/Type.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Developer",
+          "Product Designer",
+          "IoT Developer",
+          "Deep Learning Engineer",
+          "MERN Stack Developer",
+          "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
